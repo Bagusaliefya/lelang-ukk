f592053e3af7e7613abce4c54d44a68450ecdd8e
Bagus-lab <bagusalief2207@gmail.com>
2024-02-07 00:25:44 +0700

    UPDATE REGIS

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -42,14 +42,13 @@
     <main class="main-content  mt-0">
         <section class="min-vh-100 mb-8">
             <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
-                style="background-image: url('../assets/img/curved-images/curved14.jpg');">
+                style="background-image: url('../assets/img/regis.jpg');">
                 <span class="mask bg-gradient-dark opacity-6"></span>
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-lg-5 text-center mx-auto">
-                            <h1 class="text-white mb-2 mt-5">Welcome!</h1>
-                            <p class="text-lead text-white">Use these awesome forms to login or create new account in
-                                your project for free.</p>
+                            <h1 class="text-white mb-2 mt-5">SELAMAT DATANG!</h1>
+                            <p class="text-lead text-white">Silahkan idi data kalian disini ya!</p>
                         </div>
                     </div>
                 </div>
